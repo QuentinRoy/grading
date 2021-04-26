ab7302c92e7f87535a42a9d8996b9f475855c4e3
Quentin Roy <quentin@quentinroy.fr>
2021-04-26 17:52:59 -0400

    q24

--- data/solutions/q24.js
+++ data/solutions/q24.js
@@ -2,6 +2,22 @@ let rows = 10;
 let cols = 8;
 let size = 20;
 
+// Solution 1
+
+function setup() {
+  createCanvas(size * rows, size * cols);
+
+  for (let i = 0; i < rows; i++) {
+    let color = map(i, 0, rows - 1, 0, 255);
+    fill(color);
+    for (let j = 0; j < cols; j++) {
+      ellipse(i * size + size / 2, j * size + size / 2, size, size);
+    }
+  }
+}
+
+// Solution 2
+
 function setup() {
   createCanvas(size * rows, size * cols);
 
